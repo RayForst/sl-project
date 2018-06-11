9f9939173dee0224870ca68fc58021b910a895f5
Sergej Kalpakov <sergeikalpakov@gmail.com>
2018-06-12 01:14:16 +0300

    Instagram + Tweeterr

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -1,66 +1,70 @@
-const  webpackStream = require('webpack-stream')
-    , webpack = require('webpack')
+const webpackStream = require('webpack-stream'),
+  webpack = require('webpack')
 
-module.exports = function (gulp, plugins, options) {
-    let webpackPlugins = [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jquery: 'jquery',
-            jQuery: 'jquery',
-            "window.jQuery": 'jquery'
-        })
-    ];
+module.exports = function(gulp, plugins, options) {
+  let webpackPlugins = [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ]
 
-    if (options.isProd) {
-        webpackPlugins.push(
-            new webpack.optimize.UglifyJsPlugin({
-                sourceMap: false,
-                compress: {
-                    warnings: false
-                }
-            })
-        )
-    }
+  if (options.isProd) {
+    webpackPlugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: false,
+        compress: {
+          warnings: false,
+        },
+      }),
+    )
+  }
 
-    return plugins.multipipe(
-        gulp.src(options.src, {}),
-        plugins.eslint(),
-        plugins.webpack({
-            entry: {
-                // Index
-                'main': __dirname + "/../src/html/main"
+  return plugins
+    .multipipe(
+      gulp.src(options.src, {}),
+      plugins.eslint(),
+      plugins.webpack(
+        {
+          entry: {
+            // Index
+            main: __dirname + '/../src/html/main',
+          },
+          output: {
+            publicPath: options.dest,
+            filename: '[name].js',
+          },
+          watch: options.watch,
+          devtool: !options.isProd ? 'cheap-module-inline-source-map' : '',
+          module: {
+            loaders: [
+              {
+                test: /\.js$/,
+                exclude: [/html\/vendors/, /(node_modules|bower_components)/],
+                loaders: ['babel-loader?presets[]=es2015', 'eslint-loader'],
               },
-            output: {
-                publicPath: options.dest,
-                filename: '[name].js'
-            },
-            watch: options.watch,
-            devtool: !options.isProd ? 'cheap-module-inline-source-map' : '',
-            module: {
-                loaders: [{
-                    test: /\.js$/,
-                    exclude: [
-                        /js\/vendor/,
-                        /(node_modules|bower_components)/
-                    ],
-                    loaders: [
-                        'babel-loader?presets[]=es2015',
-                        'eslint-loader'
-                    ]
-                }]
-            },
-            plugins: webpackPlugins,
-            watchOptions: {
-                aggregateTimeout: 200,
-                poll: true
-            }
-        }, webpack),
-        gulp.dest(options.dest)
-    ).on('error', plugins.notify.onError(function (err) {
+            ],
+          },
+          plugins: webpackPlugins,
+          watchOptions: {
+            aggregateTimeout: 200,
+            poll: true,
+          },
+        },
+        webpack,
+      ),
+      gulp.dest(options.dest),
+    )
+    .on(
+      'error',
+      plugins.notify.onError(function(err) {
         return {
-            title: 'CSS',
-            message: err.message
+          title: 'CSS',
+          message: err.message,
         }
-    }));
-};
+      }),
+    )
+}
--- src/html/main.js
+++ src/html/main.js
@@ -1,5 +1,7 @@
 import retina from 'retinajs'
 import './content-items/header/header'
+import './content-items/social/instagram/instagram'
+import './content-items/social/tweets/tweets'
 
 $('body').on('click', 'a[href="#"]', e => e.preventDefault())
 
